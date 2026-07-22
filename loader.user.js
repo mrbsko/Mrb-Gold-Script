e5636cd28d768e9ed68246fd1821c00005c44f51
mrbsko <mennobender97@gmail.com>
2026-07-22 16:30:15 +0200

    Revise MRB Gold Edition script metadata and permissions
    Updated metadata and permissions for the MRB Gold Edition Loader script.

--- loader.user.js
+++ loader.user.js
@@ -1,13 +1,22 @@
 // ==UserScript==
-// @name         MRB Gold Edition
-// @namespace    MRB
-// @version      0.0.1
-// @description  MRB Gold Edition Loader
-// @match        https://barafranca.nl/*
-// @match        https://www.barafranca.nl/*
-// @run-at       document-start
-// @require      https://mrbsko.github.io/Mrb-Gold-Script/mrb-gold.js
-// @grant        none
+// @name         MRB Gold Edition Loader
+// @namespace    https://barafranca.nl
+// @version      11.11.17
+// @description  Laadt automatisch de nieuwste MRB Gold Edition vanaf GitHub
+// @author       Mrb
+// @include      http://*.barafranca.nl/*
+// @include      https://*.barafranca.nl/*
+// @include      http://barafranca.nl/*
+// @include      https://barafranca.nl/*
+// @run-at       document-end
+// @grant        unsafeWindow
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_notification
+// @grant        GM_xmlhttpRequest
+// @connect      maker.ifttt.com
+// @connect      script.google.com
+// @connect      script.googleusercontent.com
+// @require      https://raw.githubusercontent.com/Mrbsko/Mrb-Gold-Script/main/mrb-gold.js
 // ==/UserScript==
-
-console.log("MRB Loader gestart");
