b110d5bb8cbd9b159a1ec00d8bbc90bf1a89dbcc
mrbsko <mennobender97@gmail.com>
2026-07-22 15:59:34 +0200

    Add MRB Gold Edition UserScript loader

---
+++ loader.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         MRB Gold Edition
+// @namespace    MRB
+// @version      0.0.1
+// @description  MRB Gold Edition Loader
+// @match        https://barafranca.nl/*
+// @match        https://www.barafranca.nl/*
+// @run-at       document-start
+// @require      https://JOUWNAAM.github.io/JOUWREPOSITORY/mrb-gold.js
+// @grant        none
+// ==/UserScript==
+
+console.log("MRB Loader gestart");
